e601d9e30e069f2825c0489e07f267725e655c82
ShintaroaSuzuki <50007328+ShintaroaSuzuki@users.noreply.github.com>
2020-12-07 09:42:30 +0900

    commit

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Shintaroa';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = "Shintaroa's Blog";
 
 export default function Layout({
     children,
